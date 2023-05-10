8988b2cf4e56c035b3e569a791a129d02589b6e4
Vicente Guzman <luciomsp@geeks.ms>
2023-05-10 14:19:46 -0600

    v1

---
+++ ChatGPT Bot/ChatGPTClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace ChatGPT_Bot
+{
+    public class ChatGPTClient
+    {
+        private readonly string _apiKey;
+        private readonly RestClient _client;
+
+        // Constructor que toma la clave API como parámetro
+        public ChatGPTClient(string apiKey)
+        {
+            _apiKey = apiKey;
+            // Inicializar el RestClient con el extremo de la API de ChatGPT
+            _client = new RestClient("https://api.openai.com/v1/engines/text-davinci-003/completions");
+        }
+
+        // Agregaremos métodos aquí para interactuar con la API.
+
+        // Método para enviar un mensaje a la API de ChatGPT y devolver la respuesta
+        public string SendMessage(string message)
+        {
+            // Crear una nueva solicitud POST
+            var request = new RestRequest("", Method.Post);
+            // Establecer el encabezado de tipo de contenido (Content-Type)
+            request.AddHeader("Content-Type", "application/json");
+            // Establecer el encabezado de autorización con la clave API
+            request.AddHeader("Authorization", $"Bearer {_apiKey}");
+
+            // Crea el cuerpo de la solicitud con el mensaje y otros parámetros
+            var requestBody = new
+            {
+                prompt = message,
+                max_tokens = 100,
+                n = 1,
+                stop = (string?)null,
+                temperature = 0.7,
+            };
+
+            // Agrega el cuerpo JSON a la solicitud
+            request.AddJsonBody(JsonConvert.SerializeObject(requestBody));
+
+            // Ejecutar la solicitud y recibir la respuesta
+            var response = _client.Execute(request);
+
+            // Deserializar el contenido JSON de la respuesta
+            var jsonResponse = JsonConvert.DeserializeObject<dynamic>(response.Content ?? string.Empty);
+
+            // Extrae y devuelve el texto de respuesta del chatbot
+            return jsonResponse?.choices[0]?.text?.ToString()?.Trim() ?? string.Empty;
+        }
+    }
+}
---
+++ ChatGPT Bot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ChatGPT_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Añada su clave API de ChatGPT
+            string apiKey = "tu CLAVE aquí";
+            // Crea una instancia de ChatGPTClient con la clave API
+            var chatGPTClient = new ChatGPTClient(apiKey);
+
+            // Muestra un mensaje de bienvenida
+            Console.WriteLine("Hola amigX, bienvenidX al chatbot de ChatGPT. Si quieres escapar, solo escribe 'salir'...");
+
+            // Ingrese un ciclo para tomar la entrada del usuario y mostrar las respuestas del chatbot
+            while (true)
+            {
+                // Solicitar al usuario la entrada
+                Console.ForegroundColor = ConsoleColor.Green; // Establecer el color del texto en verde
+                Console.Write("Tú: ");
+                Console.ResetColor(); // Restablece el color del texto a su valor predeterminado
+                string input = Console.ReadLine() ?? string.Empty;
+
+                // Salir del bucle si el usuario escribe "salir"
+                if (input.ToLower() == "salir")
+                    break;
+
+                // Envia la entrada del usuario a la API de ChatGPT y recibe una respuesta
+                string response = chatGPTClient.SendMessage(input);
+
+                // Se muestra la respuesta del chatbot
+                Console.ForegroundColor = ConsoleColor.Blue; // Establecer el color del texto en azul
+                Console.Write("Chatbot: ");
+                Console.ResetColor(); // Restablece el color del texto a su valor predeterminado
+                Console.WriteLine(response);
+            }
+        }
+    }
+}
